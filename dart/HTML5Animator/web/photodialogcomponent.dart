fe1516bbcd9576f1aa919a030fef42c2be9919a7
Eric Barndollar <ebarn@dhcp-172-19-32-167.mtv.corp.google.com>
2013-04-04 11:28:53 -0700

    Makes photo dialog functional for adding image URL

--- dart/HTML5Animator/web/photodialogcomponent.dart
+++ dart/HTML5Animator/web/photodialogcomponent.dart
@@ -1,7 +1,9 @@
 library photodialog;
 
+import 'dart:async'
 import 'dart:html';
 import 'html5animator.dart';
+import 'imagepalette.dart';
 import 'modaldialogcomponent.dart';
 import 'package:web_ui/web_ui.dart';
 
@@ -9,8 +11,18 @@ import 'package:web_ui/web_ui.dart';
 /** Upload photo dialog. */
 class PhotoDialogComponent extends WebComponent {
   void created() {
+    // Workarount web_ui bug passing function as template param...
     _root.query('#photoDialog').xtag.onCloseFn = handleClose;
   }
   
   handleClose() => movieState.isPhotoDialogShowing = false;
+  
+  addPhoto() {
+    InputElement inputEl = query('#photoUrlInput');
+    addUnsafeUrlImageToPalette(inputEl.value);
+    
+    // Close dialog.
+    new Timer(const Duration(milliseconds: 500),
+        () => _root.query('#photoDialog').xtag.handleClose());
+  }
 }
