2813cba8a28d6c3e1adbe1dc9cb1a03e387a3524
Eric Barndollar <ebarn@dhcp-172-19-32-167.mtv.corp.google.com>
2013-04-03 19:05:57 -0700

    Style updates and adding initial play controls component

---
+++ dart/HTML5Animator/web/playcontrolscomponent.dart
@@ -0,0 +1,14 @@
+library timeline;
+
+import 'dart:html';
+import 'html5animator.dart';
+import 'package:web_ui/watcher.dart' as watcher;
+import 'package:web_ui/web_ui.dart';
+
+
+/** Play controls to play, pause, and jump to beginning or end. */
+class PlayControlsComponent extends WebComponent {  
+  /** Called once this component enters the DOM. */
+  void inserted() {
+  }
+}
